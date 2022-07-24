24c24c8c149c348f763b3b7438fbb34629a8f352
TheCourierNV <andreascart04@gmail.com>
2022-07-24 20:47:40 +0200

    Very basic program to check all the required libraries
    We use GLFW to create an empty window, we use Vulkan to enumerate the
    existing extensions and we muliply a matrix by a vector to check glm

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,39 @@
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/vec4.hpp>
+#include <glm/mat4x4.hpp>
+
 #include <iostream>
 #include "Version.h"
 
 int main(int, char**) {
     std::cout << "Hello, world!" << std::endl;
     std::cout << VERSION_MAJOR << "." << VERSION_MINOR << "." << VERSION_PATCH << std::endl;
+
+    glfwInit();
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+
+    GLFWwindow *window = glfwCreateWindow(800, 600, "Vulkan Window", nullptr, nullptr);
+
+    uint32_t extensionsCount = 0;
+    vkEnumerateInstanceExtensionProperties(nullptr, &extensionsCount, nullptr);
+
+    std::cout << "Extensions supported: " << extensionsCount << std::endl;
+
+    glm::mat4 matrix;
+    glm::vec4 vec;
+
+    auto test = matrix * vec;
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+
+    glfwTerminate();
 }
